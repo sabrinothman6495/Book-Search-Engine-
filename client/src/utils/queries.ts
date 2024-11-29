a237a8639a4a81554a0e3691d1772822268dea8a
sabrinothman95 <68713978+sabrinothman95@users.noreply.github.com>
2024-11-28 20:25:27 -0500

    updated query again pls run buddy

--- client/src/utils/queries.ts
+++ client/src/utils/queries.ts
@@ -16,15 +16,15 @@ export const GET_ME = gql`
   
 `;
 //added to resolve issues with "searchBooks" query
-export const SEARCH_BOOKS = gql`
-  query searchBooks($query: String!) {
-    searchBooks(query: $query) {
-      bookId
-      authors
-      description
-      title
-      image
-      link
-    }
-  }
-`;
+// export const SEARCH_BOOKS = gql`
+//   query searchBooks($query: String!) {
+//     searchBooks(query: $query) {
+//       bookId
+//       authors
+//       description
+//       title
+//       image
+//       link
+//     }
+//   }
+// `;
