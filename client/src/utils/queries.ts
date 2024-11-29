11b56756527b3215e39910662a4910fcc9298681
sabrinothman95 <68713978+sabrinothman95@users.noreply.github.com>
2024-11-28 20:36:15 -0500

    messin with them queries

--- client/src/utils/queries.ts
+++ client/src/utils/queries.ts
@@ -3,18 +3,18 @@ import { gql } from '@apollo/client';
 
 
 export const GET_ME = gql`
-   {
+  query GetMe {
     me {
-    bookId
-    authors
-    description
-    title
-    image
-    link
-      }
+      bookId
+      authors
+      description
+      title
+      image
+      link
     }
-  
+  }
 `;
+
 //added to resolve issues with "searchBooks" query
 export const SEARCH_BOOKS = gql`
   query searchBooks($query: String!) {
